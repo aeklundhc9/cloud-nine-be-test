d748f6a67d46d17f8239ca2c851f0b48c277f3ca
Albin Eklundh <albin.eklundh@cloudnine.se>
2023-02-26 18:30:32 +0100

    Refactor out dynamics from Spotify API responses

---
+++ TechTestBackend/Integration/SpotifyOauthTokenApiModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+#pragma warning disable CS8618
+
+namespace TechTestBackend.Integration;
+
+public class SpotifyOauthTokenApiModel
+{
+    [JsonProperty("access_token")]
+    public string AccessToken { get; set; }
+}
---
+++ TechTestBackend/Integration/SpotifyTrackApiModel.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS8618
+
+namespace TechTestBackend.Integration;
+
+public class SpotifyTrackApiModel
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+}
